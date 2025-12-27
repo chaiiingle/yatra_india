aa584352adb5d7727dba3345ed7aa75dbf948329
chaiiingle <chaitali.ingle_it25@pccoer.in>
2025-12-27 21:12:13 +0530

    Create supabaseClient.ts
    MYYY DATAAABASEEEEE

---
+++ supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+// You get these from your Supabase Dashboard Settings > API
+const supabaseUrl = 'https://lfudkqlfmcspgiqdzyql.supabase.co'
+const supabaseAnonKey = 'lfudkqlfmcspgiqdzyql'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
