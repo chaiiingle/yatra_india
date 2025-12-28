a97b5ee2080213e3cae2169214b1cf733da3c801
chaiiingle <chaitali.ingle_it25@pccoer.in>
2025-12-28 18:03:26 +0530

    Update supabaseClient.ts
    im so silly heheh

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // You get these from your Supabase Dashboard Settings > API
 const supabaseUrl ='https://lfudkqlfmcspgiqdzyql.supabase.co';
-const supabaseAnonKey ='lfudkqlfmcspgiqdzyql';
+const supabaseAnonKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmdWRrcWxmbWNzcGdpcWR6eXFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY4Mzg5NjYsImV4cCI6MjA4MjQxNDk2Nn0.UyQ3qqGXmp229maRwCMD_zZ1_h2so4U1s4BZf3voidE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
